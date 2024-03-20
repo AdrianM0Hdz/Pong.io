d6fd9f2ef8bb74db4aa19589ce1eaf4f2862c1fc
A01284610 <A01284610@tec.mx>
2024-03-20 10:42:44 -0600

    Agregar eje en x, colores

--- pongIO.py
+++ pongIO.py
@@ -21,23 +21,28 @@ def value():
     """Randomly generate value between (-5, -3) or (3, 5)."""
     return (3 + random() * 2) * choice([1, -1])
 
-
 ball = vector(0, 0)
-aim = vector(value(), value())
-state = {1: 0, 2: 0}
+aim = vector(5, 5)
+state = {1: (0,0), 2: (0,0)}
 
 
-def move(player, change):
+def move(player, change, direction):
     """Move player position by change."""
-    state[player] += change
+    if (direction == 1):
+        state[player] = (state[player][0],state[player][1]+change)
+    else:
+        state[player] = (state[player][0]+change,state[player][1])
 
 
-def rectangle(x, y, width, height):
+
+def rectangle(x, y, width, height, fillcolor):
     """Draw rectangle at (x, y) with given width and height."""
     up()
     goto(x, y)
     down()
+    color(fillcolor)
     begin_fill()
+    
     for count in range(2):
         forward(width)
         left(90)
@@ -49,8 +54,8 @@ def rectangle(x, y, width, height):
 def draw():
     """Draw game and move pong ball."""
     clear()
-    rectangle(-200, state[1], 10, 50)
-    rectangle(190, state[2], 10, 50)
+    rectangle(-200-state[1][1], state[1][0], 10, 50, 'orange')
+    rectangle(190-state[2][1], state[2][0], 10, 50, 'blue')
 
     ball.move(aim)
     x = ball.x
@@ -58,29 +63,37 @@ def draw():
 
     up()
     goto(x, y)
+    color('black')
+
+        
     dot(10)
     update()
 
     if y < -200 or y > 200:
         aim.y = -aim.y
 
-    if x < -185:
-        low = state[1]
-        high = state[1] + 50
+    if x < -185-state[1][1] and x > -200-state[1][1]:
+        low = state[1][0]
+        high = state[1][0] + 50
 
         if low <= y <= high:
             aim.x = -aim.x
-        else:
-            return
+    
+    
+    if x < -190:
+        return
+       
 
-    if x > 185:
-        low = state[2]
-        high = state[2] + 50
+    if x > 185-state[2][1] and x < 200-state[2][1]:
+        low = state[2][0]
+        high = state[2][0] + 50
 
         if low <= y <= high:
             aim.x = -aim.x
-        else:
-            return
+            
+
+    if x > 190:
+        return
 
     ontimer(draw, 50)
 
@@ -89,9 +102,14 @@ setup(420, 420, 370, 0)
 hideturtle()
 tracer(False)
 listen()
-onkey(lambda: move(1, 20), 'w')
-onkey(lambda: move(1, -20), 's')
-onkey(lambda: move(2, 20), 'i')
-onkey(lambda: move(2, -20), 'k')
+onkey(lambda: move(1, 20, 0), 'w')
+onkey(lambda: move(1, 20, 1), 'a')
+onkey(lambda: move(1, -20, 0), 's')
+onkey(lambda: move(1, -20, 1), 'd')
+onkey(lambda: move(2, 20, 0), 'i')
+onkey(lambda: move(2, 20, 1), 'j')
+onkey(lambda: move(2, -20, 0), 'k')
+onkey(lambda: move(2, -20, 1), 'l')
+
 draw()
 done()
